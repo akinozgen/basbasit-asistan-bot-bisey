f29c0052e1d74f35b7c8fbb316922bed9a501489
Akın Özgen <akinozgen17@outlook.com>
2023-04-08 18:07:33 +0300

    döviz işlevi eklendi

--- src/getExchangeRates.js
+++ src/getExchangeRates.js
@@ -1,3 +1,18 @@
-export function getExchangeRates() {
-  return `Bilmiyorum, ama benim için en önemli şey, seninle birlikte olmaktı.`;
+import axios from 'axios';
+
+export async function getExchangeRates() {
+  const endpoint = 'https://api.exchangerate.host/latest';
+
+  const response = await axios.get(endpoint);
+
+  const { TRY, USD, BTC } = response.data.rates;
+
+  // values in EUR base. BTC, USD and EUR in TRY base
+  const tryToEur = TRY;
+  const tryToUsd = TRY / USD;
+  const tryToBtc = TRY / BTC;
+
+  return `1 TL, ${tryToEur.toFixed(2)} Euro, ${tryToUsd.toFixed(2)} Dolar ve ${tryToBtc.toFixed(
+    2
+  )} BTC`;
 }
